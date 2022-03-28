8f9c0c8336e1746530714dabde0eb8562fdf7955
Shammyshanks <thorinbriggz@gmail.com>
2022-03-28 14:32:00 -0700

    Update init.el

--- init.el
+++ init.el
@@ -20,7 +20,16 @@
 (setq use-package-always-ensure t)
 
 (use-package nix-mode
-  :mode "\\.nix\\'")
+  :mode ("\\.nix\\'" "\\.nix.in\\'"))
+(use-package nix-drv-mode
+  :ensure nix-mode
+  :mode "\\.drv\\'")
+(use-package nix-shell
+  :ensure nix-mode
+  :commands (nix-shell-unpack nix-shell-configure nix-shell-build))
+(use-package nix-repl
+  :ensure nix-mode
+  :commands (nix-repl))
 
 (recentf-mode 1) ; find recent files
 (global-set-key (kbd "C-c r") 'recentf-open-files)
